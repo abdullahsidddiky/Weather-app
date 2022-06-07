d99352eba895d796ee4406e22e84bae503f3e66b
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2022-06-08 01:09:06 +0600

    m

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'weather_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'weather',
+        'USER': 'abdullahsiddiky',
+        'PASSWORD': '1245'
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
