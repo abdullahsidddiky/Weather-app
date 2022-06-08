27eee71954d235dd22af1bd545f6766c5f750251
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2022-06-09 02:30:18 +0600

    m

--- library_project/settings.py
+++ library_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lae*j#dymxdid-3#q1-n)+5#aw=q7vwjn1l#s5#9h9=i4#gtbj'
+SECRET_KEY = 'django-insecure-hr4g@)3$o(v!uz(&=4dyj$%pw5r(dvs$c)oiyu#ssptc14_a_2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
