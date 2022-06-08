1b1c681c1053112a850eae6af08feb5d55ede24d
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2022-06-09 02:16:19 +0600

    M

--- WeatherApp/views.py
+++ WeatherApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 # Create your views here.
 def index(request):
+    ce0c01bdbd2978586a06f55294ccd379
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={Dhaka}&appid={ce0c01bdbd2978586a06f55294ccd379}'
+
+    print(url)
     return render(request,'weather/index.html')
